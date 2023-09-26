3db78df47291f5a8da99a5f45bb8fbe0bf88ca4c
Yadier <yadierg7@gmail.com>
2023-09-26 12:43:51 -0400

    added edit functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import { CommentMap } from "./components/CommentMap";
-import { ReplyIput } from "./components/ReplyInput";
 function App() {
   return (
     <main className="flex justify-center items-center">
--- src/components/UserReply.jsx
+++ src/components/UserReply.jsx
@@ -1,14 +1,10 @@
 import { currentUser as data } from "../../data.json";
 import { useState } from "react";
 
-export const UserReply = ({
-  userReply,
-  isReply,
-  setUserReply,
-  replyText,
-  setReplyText,
-}) => {
+export const UserReply = ({ isReply, replyText, setReplyText }) => {
   const [userScore, setUserScore] = useState(0);
+  const [editedText, setEditedText] = useState("");
+  const [edit, setEdit] = useState(false);
 
   const addOneHandler = () => {
     setUserScore((prev) => prev + 1);
@@ -17,6 +13,19 @@ export const UserReply = ({
   const subtractOneHandler = () => {
     setUserScore((prev) => prev - 1);
   };
+
+  const handleEdit = (index) => {
+    setEdit(true);
+    setEditedText(replyText[index]);
+  };
+
+  const handleSave = (index) => {
+    const updatedReplyText = [...replyText];
+    updatedReplyText[index] = editedText;
+    setReplyText(updatedReplyText);
+    setEdit(false);
+  };
+
   const handleDelete = (index) => {
     const updatedReplyText = [...replyText];
     updatedReplyText.splice(index, 1);
@@ -33,31 +42,67 @@ export const UserReply = ({
             <span className="flex items-center gap-4">
               <img src={data.image.png} className="w-9" alt="" />
               <p className="font-bold">{data.username}</p>
+              {/* TODO: add time when user create comment */}
               <p>created at {}</p>
             </span>
             <div>
-              <p>{item}</p>
+              {edit ? (
+                <textarea
+                  type="text"
+                  value={editedText}
+                  onChange={(e) => setEditedText(e.target.value)}
+                  cols="30"
+                  rows="3"
+                  className="border-2 w-full resize-none rounded-md "
+                />
+              ) : (
+                <p>{item}</p>
+              )}
+
               <div className="flex items-center justify-between pt-5">
                 <span className="flex p-3 bg-purple-100 rounded-lg gap-2 items-center">
                   <button onClick={addOneHandler}>+</button>
                   <p className="font-bold">{userScore}</p>
                   <button onClick={subtractOneHandler}>-</button>
                 </span>
+
+                {/* TODO: Make a custom component */}
                 <div className="flex gap-8">
-                  <button
-                    className="text-red-700 flex items-center gap-4 font-bold"
-                    onClick={() => handleDelete(index)}
-                  >
-                    <img src="./icon-delete.svg" alt="" className="w-4 h-4" />
-                    Delete
-                  </button>
-                  <button
-                    className=" flex items-center gap-4 text-[#5358b6] font-bold"
-                    onClick={() => setUserReply(!userReply)}
-                  >
-                    <img src="./icon-reply.svg" alt="" />
-                    Reply
-                  </button>
+                  {edit ? (
+                    <button
+                      className="text-[#5358b6] flex items-center gap-4 font-bold"
+                      onClick={() => handleSave(index)}
+                    >
+                      <img src="./icon-save.svg" alt="" className="w-4 h-4" />
+                      Save
+                    </button>
+                  ) : (
+                    <button
+                      className="text-red-700 flex items-center gap-4 font-bold"
+                      onClick={() => handleDelete(index)}
+                    >
+                      <img src="./icon-delete.svg" alt="" className="w-4 h-4" />
+                      Delete
+                    </button>
+                  )}
+
+                  {edit ? (
+                    <button
+                      className=" flex items-center gap-4 text-[#5358b6] font-bold"
+                      onClick={() => setEdit(!edit)}
+                    >
+                      <img src="./icon-reply.svg" alt="" />
+                      Cancel
+                    </button>
+                  ) : (
+                    <button
+                      className=" flex items-center gap-4 text-[#5358b6] font-bold"
+                      onClick={() => handleEdit(index)}
+                    >
+                      <img src="./icon-reply.svg" alt="" />
+                      Edit
+                    </button>
+                  )}
                 </div>
               </div>
             </div>
